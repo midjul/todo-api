b4c0434933a7f6a59f3aa0ad4ed04df265425ced
Midhet <djulovic_m@hotmail.com>
2017-11-20 22:41:53 +0100

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -22,8 +22,14 @@ app.post("/todos", (req, res) => {
   );
 });
 
+app.get("/todos", (req, res) => {
+  Todo.find({})
+    .then(todos => res.json({ todos }))
+    .catch(e => res.status(400).send(e));
+});
+
 app.listen(3000, () => console.log("Started on port 3000"));
 
-module.exports={
+module.exports = {
   app
-}
+};
