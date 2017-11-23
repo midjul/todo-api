365b9e5f922d75c83a37f90e181fb17fc46e9cc3
Midhet <djulovic_m@hotmail.com>
2017-11-23 21:37:12 +0100

    Refactor /todos/:id route

--- server/server.js
+++ server/server.js
@@ -31,17 +31,15 @@ app.get("/todos", (req, res) => {
 
 app.get("/todos/:id", (req, res) => {
   const id = req.params.id;
-  if (!ObjectID.isValid(id)) {
-    return res.status(404).json({});
-  }
-  Todo.findById(id).then(
-    doc => {
-      if (!doc) return res.status(404).json({});
+  if (!ObjectID.isValid(id)) return res.status(404).json({});
 
-      res.json(doc);
-    },
-    err => res.status(400).json({})
-  );
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) return res.status(404).json({});
+
+      res.json({ todo });
+    })
+    .catch(err => res.status(400).json({}));
 });
 
 app.listen(3000, () => console.log("Started on port 3000"));
